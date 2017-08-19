09b650149a2e23d6418b1bb83d963b85dc8bbf5c
ysekky <s.yoshi.mobile.1123@gmail.com>
2017-08-19 12:10:57 +0900

    refactor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
-import pandas as pd
-import gettweets
-import textanalysis
+from gettweets import gettweets
+from textanalysis import PNAnalyzer
 
 # ツイート取得のための変数
 # 2017年度隅田川花火大会の三日前9:00amから大会翌朝9:00までのtweetを取得するパラメタ
@@ -11,21 +10,16 @@ q = '#隅田川花火大会 OR 隅田川花火大会 OR (隅田川 AND 花火) O
 fname = '7_27to7_30.csv'
 pn_dict_pass = './pn_table.csv'
 
-if __name__ == __main___:
-  df_tweets = gettweets(since, until, q, fname)
+if __name__ == '__main___':
+    df_tweets = gettweets(since, until, q, fname)
   
-  # 
-  mean_list =[]
-  ta = textanalysis(pn_dict_pass)
-  for tweet in df_tweets['text']:  
-    dl_old = ta.get_diclist(tweet)
-    dl_new = ta.add_pnvalue(dl_old)
-    pnmean = ta.get_mean(dl_new)
-    means_list.append(pnmean)
-  mean_list = np.array(mean_list)
-    
-    
-  mean = means_list.mean()
-  print('average PN value since ' + since + ' to ' + until + ' is: ' + str(mean))
+    mean_list = []
+    analyzer = PNAnalyzer(pn_dict_pass)
+    for tweet in df_tweets['text']:
+        pnmean = analyzer.get_pn_mean(tweet)
+        mean_list.append(pnmean)
+    mean_list = np.array(mean_list)
+    mean = mean_list.mean()
+    print('average PN value since ' + since + ' to ' + until + ' is: ' + str(mean))
   
   
--- textanalysis.py
+++ textanalysis.py
@@ -1,56 +1,40 @@
 import pandas as pd
-import numpy  as np
+import numpy as np
 import re
 import MeCab
 
-class textanalysis:
+
+class PNAnalyzer:
 
     # 初期化．辞書のデータフレームを作成．引数はパス
-    def __init__(self, pn_dict_pass):
-      self.pn_df = pd.read_csv(pn_dict_pass, sep=':', encoding='utf-8', \
-                                      names=('Word','Reading','POS', 'PN'))
-      # 各列の分割
-      self.word_list = list(self.pn_df['Word'])
-      self.pn_list   = list(self.pn_df['PN'])
-      self.pn_dict   = dict(zip(self.word_list, self.pn_list))
+    def __init__(self, pn_dict_path):
+        self.pn_dict = self._create_pn_dict(pn_dict_path)
 
-      # MeCabインスタンスの作成．引数を無指定にするとIPA辞書になります．
-      self.m = MeCab.Tagger('')
-    
-  # テキストを形態素解析し辞書のリストを返す関数
-  def get_diclist(self, text):
-    parsed = self.m.parse(text)      # 形態素解析結果（改行を含む文字列として得られる）
-    lines = parsed.split('\n')  # 解析結果を1行（1語）ごとに分けてリストにする
-    lines = lines[0:-2]         # MeCabの返す後ろ2行は不要なので削除
-    self.diclist = []
-    for word in lines:
-      l = re.split('\t|,',word)  # 各行はタブとカンマで区切られてるので
-      d = {'Surface':l[0], 'POS1':l[1], 'POS2':l[2], 'BaseForm':l[7]}
-      self.diclist.append(d)
-    return self.diclist
+        # MeCabインスタンスの作成．引数を無指定にするとIPA辞書になります．
+        self.m = MeCab.Tagger('')
 
-  # 形態素解析結果の単語ごとのdictデータにPN値を追加する関数
-  def add_pnvalue(self, diclist_old):
-    self.diclist_new = []
-    for word in diclist_old:
-      base = word['BaseForm']        # 個々の辞書から基本形を取得
-      if base in self.pn_dict:
-        pn = float(self.pn_dict[base]) 
-      else:
-        pn = 'notfound'            # その語がPN Tableになかった場合
-        word['PN'] = pn
-        self.diclist_new.append(word)
-    return self.diclist_new
+    def _create_pn_dict(self, pn_dict_path):
+        pn_df = pd.read_csv(pn_dict_path, sep=':', encoding='utf-8',
+                            names=('Word', 'Reading', 'POS', 'PN'))
+        word_list = list(pn_df['Word'])
+        pn_list = list(pn_df['PN'])
+        return dict(zip(word_list, pn_list))
 
-  # 各ツイートのPN平均値を求める
-  def get_mean(self, dictlist):
-    pn_list = []
-    for word in dictlist:
-      pn = word['PN']
-      if pn!='notfound':
-        pn_list.append(pn)
-    if len(pn_list)>0:
-      pnmean = np.mean(pn_list)
-    else:
-      pnmean=0
-    return pnmean
+    def get_pnvalue_list(self, text):
+        parsed = self.m.parse(text)      # 形態素解析結果（改行を含む文字列として得られる）
+        lines = parsed.split('\n')  # 解析結果を1行（1語）ごとに分けてリストにする
+        lines = lines[0:-2]         # MeCabの返す後ろ2行は不要なので削除
+        pn_value_list = []
+
+        for word in lines:
+            l = re.split('\t|,', word)  # 各行はタブとカンマで区切られてるので
+            base = l[7]
+            if base not in self.pn_dict:
+                continue
+            pn_value_list.append(float(self.pn_dict[base]))
+
+        return pn_value_list
+
+    def get_pn_mean(self, text):
+        pn_value_list = self.get_pnvalue_list(text)
+        return np.mean(pn_value_list)
